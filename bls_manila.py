00066a2e85b2d3b925a9f41f0146666e611351a1
Hyun2 <hks0284@gmail.com>
2020-07-10 14:39:16 +0000

    0710 금요일 저녁

--- bls_manila.py
+++ bls_manila.py
@@ -4,44 +4,42 @@ import requests
 
 class BlsNas:
     BASE = 'http://10.254.1.112/share/v2'
-
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-OpenStack-Manila-API-Version': '2.55',
+    }
 
     def __init__(self):
         pass
 
+    def versions(self, token):
+        url = '%s/' % (self.BASE)
+        print(url)
+        self.headers['X-Auth-Token'] = token
+        return requests.get(url, headers=self.headers)
+
     def create(self, token, project_id):
         url = '%s/%s/port_forwardings' % (self.BASE, project_id)
-        headers = {
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-Auth-Token': token
-        }
-        return requests.post(url, headers=headers)
+        self.headers['X-Auth-Token'] = token
+        return requests.post(url, headers=self.headers)
 
     def list(self, token, project_id):
         # /v2/{project_id}/shares
         url = '%s/%s/shares' % (self.BASE, project_id)
-        headers = {
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-Auth-Token': token
-        }
-        return requests.get(url, headers=headers)
+        print(url)
+        self.headers['X-Auth-Token'] = token
+        return requests.get(url, headers=self.headers)
 
     def delete(self, token, project_id, port_forwarding_id):
         # /v2.0/floatingips/{floatingip_id}/port_forwardings/{port_forwarding_id}
         url = '%s/%s/port_forwardings/%s' % (self.BASE, project_id,
                                              port_forwarding_id)
-        headers = {
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-Auth-Token': token
-        }
-        return requests.delete(url, headers=headers)
+        self.headers['X-Auth-Token'] = token
+        return requests.delete(url, headers=self.headers)
 
     def update(self, token, project_id, params):
         # /v2.0/floatingips/{floatingip_id}/port_forwardings/{port_forwarding_id}
 
         url = '%s/%s/port_forwardings' % (self.BASE, project_id)
-        headers = {
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-Auth-Token': token
-        }
-        return requests.put(url, headers=headers, data=params)
+        self.headers['X-Auth-Token'] = token
+        return requests.put(url, headers=self.headers, data=params)
